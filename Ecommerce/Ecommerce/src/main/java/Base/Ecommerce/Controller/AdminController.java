207db5f613191371ef9627c9b96d3df39e505bc1
Theo Pelegrina <71673834+theit0@users.noreply.github.com>
2024-03-25 14:07:02 -0300

    first commit

---
+++ Ecommerce/Ecommerce/src/main/java/Base/Ecommerce/Controller/AdminController.java
@@ -0,0 +1,20 @@
+package Base.Ecommerce.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/admin",produces = MediaType.APPLICATION_JSON_VALUE)
+public class AdminController {
+
+    @GetMapping(value = "")
+    public ResponseEntity<?> publicEndpoint() {
+        return ResponseEntity.status(HttpStatus.OK).body("{ \"message\": \"Este es un endpoint de administrador. Podes ver esta respuesta porque tu usuario tiene el rol 'adminstrador'\"}");
+    }
+
+}
